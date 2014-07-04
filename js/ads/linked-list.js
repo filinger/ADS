f401d4d7f427a1f105a852fa8ec7a4b98fa8478c
Filinger <mathematics.max@gmail.com>
2014-07-04 13:46:36 +0400

    Added LinkedList implementation

--- js/ads/linked-list.js
+++ js/ads/linked-list.js
@@ -1,3 +1,31 @@
+function LinkedList() {
+    this.start = null;
+    this.end = null;
+    this.add = function (data) {
+        if (this.start === null) {
+            this.start = LinkedList.makeNode();
+            this.end = this.start;
+        }
+        else {
+            this.end.next = LinkedList.makeNode();
+            this.end = this.end.next;
+        }
+        this.end.data = data;
+    };
+}
+
+LinkedList.makeNode = function () {
+    return {data: null, next: null};
+};
+
+LinkedList.fromArray = function (array) {
+    var list = new LinkedList();
+    for (var i = 0; i < array.length; ++i) {
+        list.add(array[i]);
+    }
+    return list;
+};
+
 var linkedListService = function () {
     var toMember = function (value) {
         if (value !== undefined && value !== null && !isNaN(value)) {
@@ -8,6 +36,9 @@ var linkedListService = function () {
     };
 
     this.sum = function (left, right) {
+        var leftList = LinkedList.fromArray(left);
+        var rightList = LinkedList.fromArray(right);
+
         var result = [];
         for (var i = 0; i < left.length || i < right.length; ++i) {
             var leftMember = toMember(left[i]);
